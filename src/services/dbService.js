16068a451da422e8a57f7d5085882d4d4371167b
MilitaryCheese <keshinijaya@gmail.com>
2024-05-16 15:53:48 +1000

    16 may

--- src/services/dbService.js
+++ src/services/dbService.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 // Define the database URL to connect to.
-const mongoDB = "mongodb://127.0.0.1/my_database";
+const mongoDB = "mongodb://mongo/my_database";
 
 // Wait for database to connect, logging an error if there is a problem
 main().catch((err) => console.log(err));
